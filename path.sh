fafdc20531881f1c6bd8b16d710189ac564ce199
Carlson <JDCarlson@mednet.ucla.edu>
2020-03-23 15:19:51 -0700

    needed some paths to Python libraries

--- path.sh
+++ path.sh
@@ -1,4 +1,6 @@
 # -*- shell-script -*-
 
 PATH+=":/cygdrive/c/tools/Anaconda3/"
+PATH+=":/cygdrive/c/tools/Anaconda3/scripts"
+PATH+=":/cygdrive/c/tools/Anaconda3/Library/bin"
 PATH+=":/cygdrive/c/Program Files (x86)/Nmap"
