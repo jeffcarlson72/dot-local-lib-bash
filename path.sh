22aea44b81e1bd3f96b4f991dc5a2957ba1412ee
Jeff Carlson <jeff@ultimateevil.org>
2020-03-26 11:43:43 -0700

    minor style fix and bail-out if some files are used in the wrong environment

--- path.sh
+++ path.sh
@@ -1,5 +1,7 @@
 # -*- shell-script -*-
 
+[ `uname -s` != CYGWIN_NT-10.0 ] && return
+
 PATH+=":/cygdrive/c/tools/Anaconda3/"
 PATH+=":/cygdrive/c/tools/Anaconda3/scripts"
 PATH+=":/cygdrive/c/tools/Anaconda3/Library/bin"
