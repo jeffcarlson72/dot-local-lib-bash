0949c1d0de73a9f74f68a8c4f4b1d7ccf3208c99
Jeff Carlson <jeff@ultimateevil.org>
2020-04-17 08:54:57 -0700

    Fix PATH for Anaconda3 Python
    In particular, BeautifulSoup had a huge problem with https without this.

--- path.sh
+++ path.sh
@@ -2,7 +2,14 @@
 
 [ `uname -s` != CYGWIN_NT-10.0 ] && return
 
-PATH+=":/cygdrive/c/tools/Anaconda3/"
-PATH+=":/cygdrive/c/tools/Anaconda3/scripts"
-PATH+=":/cygdrive/c/tools/Anaconda3/Library/bin"
+PYPATH="/cygdrive/c/tools/Anaconda3"
+PYPATH+=":/cygdrive/c/tools/Anaconda3/Library/mingw-w64/bin"
+PYPATH+=":/cygdrive/c/tools/Anaconda3/Library/usr/bin"
+PYPATH+=":/cygdrive/c/tools/Anaconda3/Library/bin"
+PYPATH+=":/cygdrive/c/tools/Anaconda3/Scripts"
+PYPATH+=":/cygdrive/c/tools/Anaconda3/bin"
+PYPATH+=":/cygdrive/c/tools/Anaconda3/condabin"
+
+PATH="$PYPATH:$PATH"
+
 PATH+=":/cygdrive/c/Program Files (x86)/Nmap"
