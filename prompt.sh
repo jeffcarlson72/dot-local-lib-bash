1dc3ddd374e025fa1491bc39d955a6c67b2c0fb0
Jeff Carlson <jeff@ultimateevil.org>
2020-05-28 10:51:35 -0700

    redesigned prompt logic

--- prompt.sh
+++ prompt.sh
@@ -7,166 +7,124 @@ case $( caller ) in
 	;;
 esac
 
-BGBLK1="$( tput setab 0 )"
-BGRED1="$( tput setab 1 )"
-BGGRN1="$( tput setab 2 )"
-BGYLO1="$( tput setab 3 )"
-BGBLU1="$( tput setab 4 )"
-BGMAG1="$( tput setab 5 )"
-BGCYA1="$( tput setab 6 )"
-BGWHT1="$( tput setab 7 )"
-BGBLK2="$( tput setab 8 )"
-BGRED2="$( tput setab 9 )"
-BGGRN2="$( tput setab 10 )"
-BGYLO2="$( tput setab 11 )"
-BGBLU2="$( tput setab 12 )"
-BGMAG2="$( tput setab 13 )"
-BGCYN2="$( tput setab 14 )"
-BGWHT2="$( tput setab 15 )"
-
-FGBLK1="$( tput setaf 0 )"
-FGRED1="$( tput setaf 1 )"
-FGGRN1="$( tput setaf 2 )"
-FGYLO1="$( tput setaf 3 )"
-FGBLU1="$( tput setaf 4 )"
-FGMAG1="$( tput setaf 5 )"
-FGCYA1="$( tput setaf 6 )"
-FGWHT1="$( tput setaf 7 )"
-FGBLK2="$( tput setaf 8 )"
-FGRED2="$( tput setaf 9 )"
-FGGRN2="$( tput setaf 10 )"
-FGYLO2="$( tput setaf 11 )"
-FGBLU2="$( tput setaf 12 )"
-FGMAG2="$( tput setaf 13 )"
-FGCYA2="$( tput setaf 14 )"
-FGWHT2="$( tput setaf 15 )"
-
-BOLD="$( tput bold )"
-RESET="$( tput sgr0 )"
-
 __ps1flash()
 {
     if [ "$1" -ne 0 ] ; then
-	echo "$FGRED2"
+	if [ $( uname -s ) == AIX ] ; then
+	    tput setf 12
+	else
+	    tput setaf 9
+	fi
     fi
 }
 
+reset="$( tput sgr0 )"
+
 if [ $( id -u ) -eq 0 ] ; then
-    user='\[${BOLD}${FGRED1}\]\u\[$RESET\]'
+    user="$( tput setaf 9 )"
 else
-    user='\[$FGCYA1\]\u\[$RESET\]'
+    user="$( tput setaf 6 )"
 fi
 
-if [ $( uname -s ) == 'CYGWIN_NT-10.0' ] ; then
-    g="/cygdrive/c/Program Files/Git"
-    PS1="[$user"
-    PS1+='@'
-    PS1+='\[$FGMAG1\]${HOSTNAME,,*}\[$RESET\]'
-    PS1+=' '
-    PS1+='\[$FGBLU2\]\W\[$RESET\]]'
-    if [ -f "$g/mingw64/share/git/completion/git-prompt.sh" ] ; then
-	. "$g/mingw64/share/git/completion/git-prompt.sh"
-	PS1+='$( __git_ps1 "(%s)" )'
-    fi
-    PS1+='\[$( __ps1flash $? )\]\$\[$RESET\] '
-    unset g
-elif [ $( uname -s ) == 'AIX' ] ; then
-    if [ ${TERM#*-} == 256color ] ; then
-	TERM=${TERM%-*}-16color
-    fi
-    # AIX colors seem to be based on BGR rather than RGB.  Endian difference?
-    PS1='\[$( tput setf 3 )\]\u\[$RESET\]'
-    PS1+='@'
-    PS1+='\[$( tput setf 11 ; tput setb 1 )\]\h\[$RESET\]'
-    PS1+=':'
-    PS1+='\[$( tput setf 9 )\]\W\[$RESET\]'
-    PS1+='\$> '
-elif [ $( uname -s ) == 'SunOS' ] ; then
-    PS1+='\u'
-    PS1+='@'
-    PS1+='\h'
-    PS1+=':'
-    PS1+='w'
-    PS1+='\$ '
-elif [ $( uname -s ) == 'FreeBSD' ] ; then
-    # BG = 220
-    # FG = 80
-    PS1='['
-    PS1+='\u'
-    PS1+='@'
-    PS1+='\h'
-    PS1+=' '
-    PS1+='\w'
-    PS1+=']'
-    PS1+='\$ '
-elif [ $( uname -s ) == 'NetBSD' ] ; then
-    # BG = 238
-    # FG = 202
-    PS1='['
-    PS1+='\u'
-    PS1+='@'
-    PS1+='\h'
-    PS1+=' '
-    PS1+='\W'
-    PS1+=']'
-    PS1+='\$ '
-elif [ $( uname -s ) == 'OpenBSD' ] ; then
-    # BG = 81
-    # FG = 21
-    PS1='['
-    PS1+='\u'
-    PS1+='@'
-    PS1+='\h'
-    PS1+=' '
-    PS1+='\W'
-    PS1+=']'
-    PS1+='\$ '
-elif [ $( uname -s ) == 'Linux' ] ; then
-    if type -t git > /dev/null ; then
-	gitversion=$( git --version | awk '{print $NF}' )
-	gitdocs=/usr/share/doc/git-$gitversion/
-    fi
-    if [ -f /etc/centos-release -o -f /etc/fedora-release ] ; then
-	br0='['
-	br1=']'
-	host='\[${FGBLU1}\]\h\[$RESET\]'
-	sep=' '
-    elif [ -f /etc/redhat-release ] ; then
-	br0='['
-	br1=']'
-	host='\[$FGRED1\]\h\[$RESET\]'
-	sep=' '
-    elif [ -f /etc/SuSE-release -o -f /etc/SUSE-brand ] ; then
-	br0='<'
-	br1='>'
-	host='\[$FGGRN1\]\h\[$RESET\]'
-	sep=' '
-    elif [ -f /etc/debian_version ] ; then
+wdir="$( tput setaf 12 )"
+br0='['
+br1=']'
+sep=' '
+end=''
+
+case $( uname -s ) in
+    AIX)
+	if [ ${TERM#*-} == 256color ] ; then
+	    TERM=${TERM%-*}-16color
+	fi
+	if [ $( id -u ) -eq 0 ]	; then
+	    user="$( tput setf 4 )"
+	else
+	    user="$( tput setf 3 )"
+	fi
+	host="$( tput setf 11 ; tput setb 1 )"
+	wdir="$( tput setf 9 )"
 	br0=''
 	br1=''
-	host='\[${FGGRN2}\]\h\[$RESET\]'
 	sep=':'
-    fi
-    if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-	debian_chroot=$(cat /etc/debian_chroot)
-    fi
-    PS1='${debian_chroot:+($debian_chroot)}'
-    PS1+="$br0"
-    PS1+="$user"
-    PS1+='@'
-    PS1+="$host"
-    PS1+="$sep"
-    PS1+='\[$FGBLU2\]\W\[$RESET\]'
-    PS1+="$br1"
-    if [ -f "$gitdocs/contrib/completion/git-prompt.sh" ] ; then
-	. "$gitdocs/contrib/completion/git-prompt.sh"
-	PS1+='$( rc=$? ; __git_ps1 "(%s)" ; exit $rc )'
-    fi
-    PS1+='\[$( __ps1flash $? )\]\$\[$RESET\] '
-    unset br0 br1 gitdocs gitversion host
+	end='>'
+	;;
+    CYGWIN_NT-10.0)
+	gitdocs="/cygdrive/c/Program Files/Git/mingw64/share/git"
+	host="$( tput setaf 5 )"
+	;;
+    Darwin)
+	host="$( tput setaf 8 )"
+	;;
+    FreeBSD)
+	host="$( tput setaf 80 ; tput setab 220 )"
+	;;
+    Linux)
+	if type -t git > /dev/null ; then
+	    if [ -d "/usr/share/doc/git/contrib/" ] ; then
+		gitdocs="/usr/share/doc/git/contrib/"
+	    else
+		gitversion=$( git --version | awk '{print $NF}' )
+		gitdocs="/usr/share/doc/git-$gitversion/contrib/"
+	    fi
+	fi
+
+	[ -f /etc/centos-release ] && ID=centos
+	[ -f /etc/os-release ] && . /etc/os-release
+	case $ID in
+	    centos|fedora|redhat|rhel)
+		host="$( tput setaf 4 )"
+		;;
+	    debian|ubuntu)
+		host="$( tput setaf 1 )"
+		br0=''
+		br1=''
+		sep=':'
+		;;
+	    opensuse*|sled|sles|suse)
+		host="$( tput setaf 2 )"
+		br0='<'
+		br1='>'
+		;;
+	esac
+
+	unset ANSI_COLOR BUG_REPORT_URL CPE_NAME HOME_URL ID ID_LIKE	\
+	      NAME PRETTY_NAME REDHAT_BUGZILLA_PRODUCT			\
+	      REDHAT_BUGZILLA_PRODUCT_VERSION REDHAT_SUPPORT_PRODUCT	\
+	      REDHAT_SUPPORT_PRODUCT_VERSION VARIANT VARIANT_ID VERSION \
+	      VERSION_ID
+	;;
+    NetBSD)
+	host="$( tput setaf 202 ; tput setab 238 )"
+	;;
+    OpenBSD)
+	host="$( tput setaf 21 ; tput setab 81 )"
+	;;
+    SunOS)
+	host="$( tput setaf 62 )"
+	;;
+esac
+
+if [ -z "${debian_chroot:-}" -a -r "/etc/debian_chroot" ]; then
+    debian_chroot=$( cat /etc/debian_chroot )
 fi
+
+PS1='${debian_chroot:+(debian_chroot)}'
+PS1+="$br0"
+PS1+="\[$user\]\u\[$reset\]"
+PS1+='@'
+PS1+="\[$host\]\h\[$reset\]"
+PS1+="$sep"
+PS1+="\[$wdir\]\W\[$reset\]"
+PS1+="$br1"
+if [ -f "$gitdocs/completion/git-prompt.sh" ] ; then
+    . "$gitdocs/completion/git-prompt.sh"
+    PS1+='$( rc=$? ; __git_ps1 "(%s)" ; exit $rc )'
+fi
+PS1+='\[$( __ps1flash $? )\]\$\[$reset\]'
+PS1+="$end "
 
 PS2='\!> '
 
 export PS1 PS2
-unset user
+unset br0 br1 end gitdocs gitversion host sep user wdir
